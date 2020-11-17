67627f69d96bdc8e9fd8076abe5f3f564ffd4a83
Tarenja <tarenya@hotmail.com>
2020-11-17 01:00:44 +0100

    pagination working

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
- <CharacterList :characters="characters" :episodes="episodes" :locations="locations" />
+  <CharacterList
+    :characters="characters"
+    :episodes="episodes"
+    :locations="locations"
+    :totalPages="totalPages"
+    :nextPage="nextPage"
+    :previousPage="previousPage"
+    :currentPage="currentPage"
+    @next-page="goToNextPage"
+    @previous-page="goToPreviousPage"
+    @select-page="goToSelectedPage"
+  />
 </template>
 
 <script>
-import CharacterList from './components/CharacterList.vue'
+import CharacterList from "./components/CharacterList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    CharacterList
+    CharacterList,
   },
   data() {
     return {
@@ -16,20 +27,23 @@ export default {
       currentPage: 1,
       nextPage: "",
       previousPage: "",
+      totalPages: 0,
       episodes: [],
-      locations: []
-    }
+      locations: [],
+    };
   },
   created() {
     this.getAllEpisodes();
     this.getAllLocations();
-    this.getInfoFromRicksApi("https://rickandmortyapi.com/api/character/").then(response => {
-      this.setCharactersAndPages(response)
-    });
+    this.getInfoFromRicksApi("https://rickandmortyapi.com/api/character/").then(
+      (response) => {
+        this.setCharactersAndPages(response);
+      }
+    );
   },
   methods: {
     async getInfoFromRicksApi(url) {
-      const response = await fetch(url)
+      const response = await fetch(url);
       const json = await response.json();
       return json;
     },
@@ -37,58 +51,98 @@ export default {
       this.characters = info.results;
       this.nextPage = info.info.next;
       this.previousPage = info.info.prev;
+      this.totalPages = info.info.pages;
     },
     getAllEpisodes() {
-      this.getInfoFromRicksApi("https://rickandmortyapi.com/api/episode/").then(res => {
-        this.episodes = res.results;
-      })
-      this.getInfoFromRicksApi("https://rickandmortyapi.com/api/episode/?page=2").then(res => {
+      this.getInfoFromRicksApi("https://rickandmortyapi.com/api/episode/").then(
+        (res) => {
+          this.episodes = res.results;
+        }
+      );
+      this.getInfoFromRicksApi(
+        "https://rickandmortyapi.com/api/episode/?page=2"
+      ).then((res) => {
         this.episodes = this.episodes.concat(res.results);
-      })
-      this.getInfoFromRicksApi("https://rickandmortyapi.com/api/episode/?page=3").then(res => {
+      });
+      this.getInfoFromRicksApi(
+        "https://rickandmortyapi.com/api/episode/?page=3"
+      ).then((res) => {
         this.episodes = this.episodes.concat(res.results);
-      })
+      });
     },
     getAllLocations() {
-      this.getInfoFromRicksApi("https://rickandmortyapi.com/api/location/").then(res => {
+      this.getInfoFromRicksApi(
+        "https://rickandmortyapi.com/api/location/"
+      ).then((res) => {
         this.locations = res.results;
-      })
-      this.getInfoFromRicksApi("https://rickandmortyapi.com/api/location/?page=2").then(res => {
+      });
+      this.getInfoFromRicksApi(
+        "https://rickandmortyapi.com/api/location/?page=2"
+      ).then((res) => {
         this.locations = this.locations.concat(res.results);
-      })
-      this.getInfoFromRicksApi("https://rickandmortyapi.com/api/location/?page=3").then(res => {
+      });
+      this.getInfoFromRicksApi(
+        "https://rickandmortyapi.com/api/location/?page=3"
+      ).then((res) => {
         this.locations = this.locations.concat(res.results);
-      })
-      this.getInfoFromRicksApi("https://rickandmortyapi.com/api/location/?page=4").then(res => {
+      });
+      this.getInfoFromRicksApi(
+        "https://rickandmortyapi.com/api/location/?page=4"
+      ).then((res) => {
         this.locations = this.locations.concat(res.results);
-      })
-      this.getInfoFromRicksApi("https://rickandmortyapi.com/api/location/?page=5").then(res => {
+      });
+      this.getInfoFromRicksApi(
+        "https://rickandmortyapi.com/api/location/?page=5"
+      ).then((res) => {
         this.locations = this.locations.concat(res.results);
-      })
-      let temp = [...new Set(this.locations)]
+      });
+      let temp = [...new Set(this.locations)];
       this.locations = temp;
-    }
+    },
+    goToNextPage() {
+      this.currentPage++;
+      this.getInfoFromRicksApi(
+        `https://rickandmortyapi.com/api/character/?page=${this.currentPage}`
+      ).then((response) => {
+        this.setCharactersAndPages(response);
+      });
+    },
+    goToPreviousPage() {
+      this.currentPage--;
+      this.getInfoFromRicksApi(
+        `https://rickandmortyapi.com/api/character/?page=${this.currentPage}`
+      ).then((response) => {
+        this.setCharactersAndPages(response);
+      });
+    },
+    goToSelectedPage(page) {
+      this.currentPage = page;
+      this.getInfoFromRicksApi(
+        `https://rickandmortyapi.com/api/character/?page=${this.currentPage}`
+      ).then((response) => {
+        this.setCharactersAndPages(response);
+      });
+    },
   },
-}
+};
 </script>
 
 <style>
-
 @font-face {
-    font-family: 'Get Schwifty';
-    src: url('./assets/GetSchwifty-Regular.eot');
-    src: url('./assets/GetSchwifty-Regular.eot?#iefix') format('embedded-opentype'),
-        url('./assets/GetSchwifty-Regular.woff2') format('woff2'),
-        url('./assets/GetSchwifty-Regular.woff') format('woff'),
-        url('./assets/GetSchwifty-Regular.ttf') format('truetype');
-    font-weight: normal;
-    font-style: normal;
-    font-display: swap;
+  font-family: "Get Schwifty";
+  src: url("./assets/GetSchwifty-Regular.eot");
+  src: url("./assets/GetSchwifty-Regular.eot?#iefix")
+      format("embedded-opentype"),
+    url("./assets/GetSchwifty-Regular.woff2") format("woff2"),
+    url("./assets/GetSchwifty-Regular.woff") format("woff"),
+    url("./assets/GetSchwifty-Regular.ttf") format("truetype");
+  font-weight: normal;
+  font-style: normal;
+  font-display: swap;
 }
 
-
 #app {
-  font-family: 'Goldman', cursive, sans-serif;
+  font-family: "Goldman", cursive, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   margin-top: 60px;
--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -1,59 +1,146 @@
 <template>
   <div>
     <h1>Rick's Biographical Nonsense</h1>
-    <div v-if="characters">
-        <div class="character" v-for="character in characters" :key="character.id">
-            <img v-if="character.image" :src="character.image" />
-            <h2 v-if="character.name">{{ character.name}}</h2>
-            <div class="character__stats">
-                <p v-if="character.gender">Gender: {{ character.gender }}</p>
-                <p v-if="character.species">Species: {{ character.species }}</p>
-                <p v-if="character.type">Type: {{ character.type }}</p>
-                <p v-if="character.status">Status: {{ character.status }}</p>
-            </div>
-            <div class="character__origin-location">
-                <h3>Origin</h3>
-                    <template v-for="location in locations" :key="location.id">
-                        <div v-if="character.origin.url === location.url">
-                            <p>Name: {{ location.name }}</p>
-                            <p>Type: {{ location.type }}</p>
-                            <p>Dimension: {{ location.dimension }}</p>
-                            <p>Residents seen: {{ location.residents.length }}</p>
-                        </div>
-                    </template>
-                <h3>Current Location</h3>
-                   <template v-for="location in locations" :key="location.id">
-                        <div v-if="character.location.url === location.url">
-                            <p>Name: {{ location.name }}</p>
-                            <p>Type: {{ location.type }}</p>
-                            <p>Dimension: {{ location.dimension }}</p>
-                            <p>Residents seen: {{ location.residents.length }}</p>
-                        </div>
-                    </template>
-            </div>
-            <ul class="character__episodes" v-if="character.episode">
-                <h3>Seen in episodes:</h3>
-                <template v-for="episode in episodes" :key="episode.id">
-                <li v-if="character.episode.includes(episode.url)">
-                    <p>{{episode.name}}</p>
-                </li>
-                </template>
-            </ul>
+    <div class="pagination">
+      <p v-if="previousPage" @click="$emit('previous-page')">Previous</p>
+      <p
+        class="pagination__number"
+        :class="{ selected: i === currentPage }"
+        v-for="i in totalPages"
+        :key="i"
+        @click="$emit('select-page', i)"
+      >
+        {{ i }}
+      </p>
+      <p v-if="nextPage" @click="$emit('next-page')">Next</p>
+    </div>
+    <div class="characters" v-if="characters">
+      <div
+        class="character"
+        v-for="character in characters"
+        :key="character.id"
+      >
+        <img v-if="character.image" :src="character.image" />
+        <h2 v-if="character.name">{{ character.name }}</h2>
+        <div class="character__stats">
+          <p v-if="character.gender"><b>Gender:</b> {{ character.gender }}</p>
+          <p v-if="character.species">
+            <b>Species:</b> {{ character.species }}
+          </p>
+          <p v-if="character.type"><b>Type:</b> {{ character.type }}</p>
+          <p v-if="character.status"><b>Status:</b> {{ character.status }}</p>
         </div>
+        <div class="character__origin-location">
+          <h3 v-if="character.origin.url">Origin</h3>
+          <template v-for="location in locations" :key="location.id">
+            <div v-if="character.origin.name === location.name">
+              <p><b>Name:</b> {{ location.name }}</p>
+              <p><b>Type:</b> {{ location.type }}</p>
+              <p><b>Dimension:</b> {{ location.dimension }}</p>
+              <p><b>Residents known:</b> {{ location.residents.length }}</p>
+            </div>
+          </template>
+          <h3 v-if="character.location.url">Current Location</h3>
+          <template v-for="location in locations" :key="location.id">
+            <div v-if="character.location.name === location.name">
+              <p><b>Name:</b> {{ location.name }}</p>
+              <p><b>Type:</b> {{ location.type }}</p>
+              <p><b>Dimension:</b> {{ location.dimension }}</p>
+              <p><b>Residents seen:</b> {{ location.residents.length }}</p>
+            </div>
+          </template>
+        </div>
+        <ul class="character__episodes" v-if="character.episode">
+          <h3><b>Seen in episodes:</b></h3>
+          <template v-for="episode in episodes" :key="episode.id">
+            <li v-if="character.episode.includes(episode.url)">
+              <p>{{ episode.id }}. {{ episode.name }}</p>
+            </li>
+          </template>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'CharacterList',
-  props: ["characters", "episodes", "locations"]
-}
+  name: "CharacterList",
+  props: [
+    "characters",
+    "episodes",
+    "locations",
+    "totalPages",
+    "nextPage",
+    "previousPage",
+    "currentPage",
+  ],
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    h1, h2, h3 {
-        font-family: 'Get Schwifty';
-    }
+h1,
+h2,
+h3 {
+  font-family: "Get Schwifty";
+  text-align: center;
+}
+
+.pagination {
+  text-align: center;
+  padding: 3rem;
+}
+
+.pagination p {
+  display: inline-block;
+  width: fit-content;
+  height: 2.5rem;
+  box-sizing: border-box;
+  margin: 0.3rem;
+  padding: 0.6rem 0.3rem 0.3rem;
+  border-radius: 3px;
+}
+
+p.pagination__number {
+  width: 2.5rem;
+}
+
+.pagination__number.selected {
+  background-color: #e5f1f0;
+}
+
+.pagination p:hover {
+  cursor: pointer;
+  background-color: #b9e6e2;
+}
+
+.characters {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+  grid-gap: 2rem;
+}
+
+.character {
+  background-color: #e5f1f0;
+  border: 1px solid black;
+  border-radius: 3px;
+  margin: 1rem;
+  padding: 1.5rem;
+  text-align: center;
+}
+
+img {
+  border-radius: 3px;
+}
+
+.character__stats,
+.character__origin-location {
+  text-align: left;
+}
+
+.character__episodes {
+  list-style: none;
+  text-align: left;
+}
 </style>
